ca316cdee574f8fd4942429efa8ddbe5ed4aed40
CHERIF2001 <cherifaziz319@gmail.com>
2024-01-29 12:28:25 +0100

    modification de game.py et constants

--- jeu_de_dame/constants.py
+++ jeu_de_dame/constants.py
@@ -6,10 +6,15 @@ SQUARE_SIZE = WIDTH//ROWS
 
 #définition des couleurs 
 
+LINE_COLOR = (0, 0, 0)
 RED = (255,0,0)
 WHITE = (255,255,255)
 BLACK =(0,0,0)
 BLUE = (0,0,255)
 GREY = (128, 128,128)
+YELLOW = (245, 255, 196)
+BORColor=(56,31,10)
+BROWN = (102,57,18)
+
 
 CROWN= pygame.transform.scale(pygame.image.load('assets/king.png'), (44,25))
