3b10cddd5c8e5a6e2355e476aca73d770d76c1ad
YannCedL <104587740+YannCedL@users.noreply.github.com>
2024-01-30 16:08:03 +0100

    Update des composants

--- jeu_de_dame/board.py
+++ jeu_de_dame/board.py
@@ -1,38 +1,160 @@
-import pygame
-#definition des dimensions de la table de jeu
-pygame.init()
-BOARD_SIZE = 8
-TILE_SIZE = 80
-WINDOW_SIZE = BOARD_SIZE * TILE_SIZE
-COLUMN = 8
-ROW = 8
-screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
-pygame.display.set_caption('DAME')
-board = [[0 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-#Definition de couleur
-LINE_COLOR = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-YELLOW = (245, 255, 196)
-BRUN = (102,57,18)
-BORColor=(56,31,10)
+import pygame 
+from .constants import *
+from .piece import *  
+
+class Board:
+	def __init__(self):
+		self.board = []
+		self.red_left = self.white_left =12 
+		self.red_kings = self.white_kings =0
+		self.create_board()
+
+	def draw_squares(self, win):
+		win.fill(BLACK)
+		for row in range(ROWS):
+			for col in range(row % 2, COLS, 2):
+				pygame.draw.rect(win, WHITE, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+	def move(self, piece, row, col):
+		self.board[piece.row][piece.col], self.board[row][col]= self.board[row][col], self.board[piece.row][piece.col]
+		piece.move(row,col)
+
+		if row == ROWS - 1 or row == 0:
+			piece.make_king()
+			if piece.color == WHITE:
+				self.white_kings +=1
+			else:
+				self.red_kings += 1
+
+	def get_piece(self, row, col):
+		return self.board[row][col]
 
 
-def draw_board():
-    screen.fill(WHITE)
-    for i in range(BOARD_SIZE):
-        for j in range(BOARD_SIZE):
-            color = YELLOW if (i + j) % 2 == 0 else BRUN
-            pygame.draw.rect(screen, color, (i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE))
-
-        pygame.draw.line(screen, LINE_COLOR, (i * TILE_SIZE, 0), (i * TILE_SIZE, WINDOW_SIZE), 1)
-        pygame.draw.line(screen, LINE_COLOR, (0, i * TILE_SIZE), (WINDOW_SIZE, i * TILE_SIZE), 1)
-
-    #bordure
-    pygame.draw.line(screen, YELLOW, (0, 0), (0, WINDOW_SIZE), 20)
-    pygame.draw.line(screen, YELLOW, (WINDOW_SIZE - 1, 0), (WINDOW_SIZE - 1, WINDOW_SIZE), 20)
-    pygame.draw.line(screen, YELLOW, (0, WINDOW_SIZE - 1), (WINDOW_SIZE, WINDOW_SIZE - 1), 20)
 
 
-    pygame.display.flip()
+
+	def create_board(self):
+		for row in range(ROWS):
+			self.board.append([])
+			for col in range(COLS):
+				if col % 2 ==((row + 1)% 2):
+					if row <3: 
+						self.board[row].append(Piece(row, col, WHITE))
+					elif row > 4:
+						self.board[row].append(Piece(row, col, RED))
+					else: 
+						self.board[row].append(0)
+				else: 
+					self.board[row].append(0)
+
+
+	def draw(self, win):
+		self.draw_squares(win)
+		for row in range(ROWS):
+			for col in range(COLS):
+				piece = self.board[row][col]
+				if piece !=0:
+					piece.draw(win)
+
+
+	def remove(self, pieces):
+		for piece in pieces:
+			self.board[piece.row][piece.col]= 0
+			if piece !=0:
+				if piece.color == RED:
+					self.red_left -=1
+				else: 
+					self.white_left-=1
+
+	def winner(self):
+		if self.red_left <=0:
+			return WHITE 
+		elif self.white_left <=0:
+			return RED 
+		
+		return None
+
+
+
+	def get_valid_moves(self, piece):
+		moves = {}
+		left = piece.col - 1
+		right = piece.col +1
+		row = piece.row 
+
+		if piece.color == RED or piece.king: 
+			moves.update(self._traverse_left(row -1, max(row-3,-1), -1, piece.color, left))
+			moves.update(self._traverse_right(row -1, max(row-3,-1), -1, piece.color, right))
+		if piece.color == WHITE or piece.king:
+			moves.update(self._traverse_left(row +1, min(row+3,ROWS), 1, piece.color, left))
+			moves.update(self._traverse_right(row +1, min(row+3,ROWS), 1, piece.color, right))
+
+		return moves
+
+
+	def _traverse_left(self, start, stop, step, color, left, skipped=[]):
+		moves ={}
+		last = []
+		for r in range(start, stop, step):
+			if left <0:
+				break 
+
+			current = self.board[r][left]
+			if current == 0:
+				if skipped and not last:
+					break
+				elif skipped:
+					moves[(r, left)] = last + skipped
+				else:
+					moves[(r, left)]= last
+
+				if last:
+					if step == -1:
+						row = max(r-3, 0)
+					else: 
+						row = min(r+3, ROWS)
+					moves.update(self._traverse_left(r+step, row, step, color, left-1, skipped=last))
+					moves.update(self._traverse_right(r+step, row, step, color, left+1, skipped=last))
+				break
+			elif current.color == color:
+				break
+			else:
+				last = [current]
+
+			left -=1
+		return moves
+
+	def _traverse_right(self,start, stop, step, color, right, skipped=[]):
+		moves ={}
+		last = []
+		for r in range(start, stop, step):
+			if right >= COLS:
+				break 
+
+			current = self.board[r][right]
+			if current == 0:
+				if skipped and not last:
+					break
+				elif skipped:
+					moves[(r, right)] = last + skipped
+				else:
+					moves[(r, right)]= last
+
+				if last:
+					if step == -1:
+						row = max(r-3, 0)
+					else: 
+						row = min(r+3, ROWS)
+					moves.update(self._traverse_left(r+step, row, step, color, right-1, skipped=last))
+					moves.update(self._traverse_right(r+step, row, step, color, right+1, skipped=last))
+				break
+
+			elif current.color == color:
+				break
+			else:
+				last = [current]
+
+			right +=1
+
+		return moves
 
--- jeu_de_dame/game.py
+++ jeu_de_dame/game.py
@@ -1,12 +1,13 @@
 import pygame 
-from .constants import * 
-from .board import Board 
+from .constants import *
+from .board import Board
 
 
-class Game: 
+class Game:
 	def __init__(self, win):
 		self._init()
 		self.win = win
+
 
 
 	def update(self):
@@ -15,8 +16,12 @@ class Game:
 		pygame.display.update()
 
 
+	"""def reset(self):
+		self.board.draw(self.win)
+		pygame.display.update() """
+
 	def _init(self):
-		self.selected = None 
+		self.selected = None
 		self.board = Board()
 		self.turn = RED 
 		self.valid_moves = {}
@@ -25,49 +30,53 @@ class Game:
 		return self.board.winner()
 
 	def reset(self):
-		self.init()
+		self._init()
 
 	def select(self, row, col):
 		if self.selected:
 			result = self._move(row, col)
-			if not result: 
-				self selected = None 
+			if not result:
+				self.selected = None
 				self.select(row, col)
+ 
+		piece = self.board.get_piece(row, col)
+		if piece !=0 and piece.color == self.turn:
+			self.selected = piece
+			self.valid_moves = self.board.get_valid_moves(piece)
+			return True
 
-			piece = self.board.get_piece(row, col)
-			if piece !=0 and piece.color == self.turn:
-				self.selected = get_piece
-				self.valid_moves = self.board.get_valid_moves(piece)
-				return True 
+		return False 
 
-			return False
+	def _move(self, row, col):
+		piece = self.board.get_piece(row, col)
+		if self.selected and piece == 0 and (row, col) in self.valid_moves:
+			self.board.move(self.selected, row, col)
+			skipped = self.valid_moves[(row, col)]
+			if skipped:
+				self.board.remove(skipped)
+			self.change_turn()
+		else: 
+			return False 
 
-
-		def _move(self, row, col):
-			piece = self.board.get_piece(row, col)
-			if self.selected and piece == 0 and (row, col) in self.valid_moves:
-				self.board.move(self.selected, row, col)
-				skipped = self.valid_moves[(row, col)]
-				if skipped:
-					self.board.remove(skipped)
-				self.change_turn()
-			else: 
-				return False
-
-			return True 
-
-
-		def draw_valid_moves(self, moves):
-			for move in moves:
-				row, col = move
-				pygame.draw.circle(self.win, BLUE, (col * SQUARE_SIZE + SQUARE_SIZE//2, row * SQUARE_SIZE + SQUARE_SIZE//2 ), 15)
+		return True 
 
 
-		def change_turn(self):
-			self.valid_moves = {}
-			if self.turn == RED:
-				self.turn = WHITE 
-			else:
-				self.turn = RED 
+	def draw_valid_moves(self, moves):
+		for move in moves:
+			row,col = move
+			pygame.draw.circle(self.win, BLUE, (col * SQUARE_SIZE + SQUARE_SIZE//2, row * SQUARE_SIZE + SQUARE_SIZE//2 ), 15)
+
+
+
+
+	def change_turn(self):
+		self.valid_moves = {}
+		if self.turn == RED:
+			self.turn = WHITE
+		else: 
+			self.turn = RED 
+
+
+
 
 
