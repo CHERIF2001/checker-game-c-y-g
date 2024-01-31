7181448f95af1e9b8108779b09a516b70cab8b2b
StephyMenda <114664801+StephyMenda@users.noreply.github.com>
2024-01-31 14:17:32 +0100

    modification du fichier piece

--- jeu_de_dame/piece.py
+++ jeu_de_dame/piece.py
@@ -1,8 +1,6 @@
-
-import pygame
 from .constants import *
 
-
+"""	Cette classe permet de gérer les pièces du damier """
 class Piece:
 	PADDING = 15
 	OUTLINE = 2
@@ -16,22 +14,15 @@ class Piece:
 		self.x =0
 		self.y =0
 		self.calc_pos()
-
-
-	"""	if self.color == RED:
-			self.direction = -1
-		else: 
-			self.direction = 1 """
-
-	
 	#calculer les positions de x et y 
 	def calc_pos(self):
 		self.x = SQUARE_SIZE* self.col + SQUARE_SIZE//2
 		self.y = SQUARE_SIZE* self.row + SQUARE_SIZE//2
-
+    #cette méthode marque une piece comme un roi
 	def make_king(self):
 		self.king = True
 
+    #Cette methode permet de dessiner une pièce
 	def draw(self, win):
 		radius = SQUARE_SIZE//2 - self.PADDING
 		pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
@@ -39,11 +30,10 @@ class Piece:
 		if self.king:
 			win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
 
+	# une methode qui permet le deplacement des pièces
 	def move(self, row, col):
 		self.row = row
 		self.col = col 
-		self.calc_pos()
+		self.calc_pos()        # met à jour la position de la pièce
 
 
-	def __repr__(self):
-		return str(self.color) 
