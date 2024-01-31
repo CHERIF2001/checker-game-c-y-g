842e0304a5efba0ae3c74b4295cb37e4d0389e05
CHERIF2001 <cherifaziz319@gmail.com>
2024-01-31 20:25:11 +0100

    modification sur constants

--- jeu_de_dame/constants.py
+++ jeu_de_dame/constants.py
@@ -11,21 +11,13 @@ WHITE = (255,255,255)
 BLACK =(0,0,0)
 BLUE = (18,52,99)
 GREY = (227,227,227)
-
-CROWN= pygame.transform.scale(pygame.image.load('image/king.jpg'), (44,25))
-
-#definition des dimensions de la table de jeu
-pygame.init()
-BOARD_SIZE = 8
-TILE_SIZE = 80
-WINDOW_SIZE = BOARD_SIZE * TILE_SIZE
-COLUMN = 8
-ROW = 8
-screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
-pygame.display.set_caption('DAME')
-board = [[0 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-#Definition de couleur
 LINE_COLOR = (0, 0, 0)
 YELLOW = (245, 255, 196)
 BRUN = (102,57,18)
 BORColor=(56,31,10)
+
+
+CROWN= pygame.transform.scale(pygame.image.load('image/king.jpg'), (44,25))
+
+
+
